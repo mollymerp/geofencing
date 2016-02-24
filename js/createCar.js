40bcf8e688abee3249d505b219d8fd269cb2b6c5
Molly Lloyd <molly@mapbox.com>
2016-02-23 16:31:04 -0800

    (fix) animation working

--- js/createCar.js
+++ js/createCar.js
@@ -19,14 +19,13 @@ function getRandomColor() {
 module.exports = function(num) {
   for (var i = 0; i < num; i++) {
     getPath().then(function(path_info) {
-      cars[j] = { inside: false, ever: false };
+      cars[++j] = { inside: false, ever: false };
       var route = turf.linestring(path_info.routes[0].geometry.coordinates);
       createCar(path_info.origin)
         .transition()
         .duration(10000)
         .attrTween('transform', translateAlong(route, j))
         .remove();
-      j++;
     });
 
   }
@@ -36,8 +35,6 @@ function createCar(origin, color) {
   var color = getRandomColor();
   var new_car = d3.select('svg')
     .append('circle')
-    .attr('r', 7)
-    .attr('fill', color)
     .attr('transform', function() {
       var pixelCoords = map.project(origin.geometry.coordinates);
       return 'translate(' + pixelCoords.x + ',' + pixelCoords.y + ')';
@@ -48,22 +45,31 @@ function createCar(origin, color) {
 function translateAlong(path, j) {
   var l = turf.lineDistance(path, 'kilometers');
   return function(d, i, a) {
+    var car = d3.select(this);
     return function(t) {
       // t is time as as % of total transition duration
       var p = turf.along(path, t * l, 'kilometers');
       if (!cars[j].inside && turf.inside(p, zone.features[0])) {
         cars[j].inside = true;
+        car.classed('inzone', true);
         increment('current');
         if (!cars[j].ever) {
+          console.log('entering');
           cars[j].ever = true;
+          car.classed('enter', true);
           increment('total');
         }
       } else if (cars[j].inside && !turf.inside(p, zone.features[0])) {
+        car.classed('inzone', false);
         cars[j].inside = false;
         decrement('current');
       }
-      if (t === 1 && turf.inside(p, zone.features[0])) {
-        decrement('current');
+      if (t === 1) {
+        delete cars[j];
+        console.log(cars);
+        if (turf.inside(p, zone.features[0])){
+          decrement('current');
+        }
       }
       var pixelCoords = map.project([p.geometry.coordinates[0], p.geometry.coordinates[1]]);
       return 'translate(' + pixelCoords.x + ',' + pixelCoords.y + ')';
@@ -74,12 +80,12 @@ function translateAlong(path, j) {
 var metrics = { 'total': total, 'current': current };
 
 function increment(metric) {
-  d3.select('.'+metric + '-vehicles').text(++metrics[metric]);
+  d3.select('.' + metric + '-vehicles').text(++metrics[metric]);
   if (metric === 'total') {
     d3.select('.total-revenue').text(metrics[metric] * 5);
   }
 }
 
 function decrement(metric) {
-  d3.select('.'+metric + '-vehicles').text(--metrics[metric]);
+  d3.select('.' + metric + '-vehicles').text(--metrics[metric]);
 }
--- js/getPath.js
+++ js/getPath.js
@@ -12,12 +12,48 @@ module.exports = function(callback) {
   return defer.promise;
 }
 
-// gets two random points within London
+
+// gets two random points along London's border
 function getStartEnd() {
-  var points = turf.random('point', 2, { bbox: [-0.02300262451171875, 51.453792970504495, -0.26882171630859375, 51.55167238691343] });
-  // returns an object with start and end properties
-  return points.features.map(function(feat) {
+  var london_border_len = turf.lineDistance(london_border, 'kilometers');
+  var points = [turf.along(london_border, Math.random() * london_border_len, 'kilometers'), turf.along(london_border, Math.random() * london_border_len, 'kilometers')];
+  return points.map(function(feat) {
     return feat.geometry.coordinates;
   })
-
 }
+
+
+var london_border = {
+  "type": "Feature",
+  "properties": {},
+  "geometry": {
+    "type": "LineString",
+    "coordinates": [
+      [-0.26882171630859375,
+        51.55252630304316
+      ],
+      [-0.02780914306640625,
+        51.55273977957101
+      ],
+      [-0.0226593017578125,
+        51.455504458873776
+      ],
+      [-0.2756881713867187,
+        51.45422084861252
+      ],
+      [-0.270538330078125,
+        51.551885867448604
+      ]
+    ]
+  }
+}
+
+// // gets two random points within London
+// function getStartEnd() {
+//   var points = turf.random('point', 2, { bbox: [-0.02300262451171875, 51.453792970504495, -0.26882171630859375, 51.55167238691343] });
+//   // returns an object with start and end properties
+//   return points.features.map(function(feat) {
+//     return feat.geometry.coordinates;
+//   })
+
+// }
